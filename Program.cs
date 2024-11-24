3b084d1d5862f6b451fa9f43e6dcf7ab6d24de8b
nlvdoanh <doanhnguyen.31231020575@st.ueh.edu.vn>
2024-11-24 12:37:26 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bai1();
+            Bai2();
+        }
+        static void Bai1()
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                if (i == 4)
+                {
+                    i++;
+                    continue;
+                }
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+        static void Bai2()
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                if (i == 7)
+                {
+                    i++;
+                    continue;
+                }
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
